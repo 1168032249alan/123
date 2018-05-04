a67016b3f92bd79d6f90275eba1cc91f9414afde
1168032249alan <36265104+1168032249alan@users.noreply.github.com>
2018-05-04 12:25:01 -0300

    estructuras genericas v2
    falta 8 puntos xd

--- main.c
+++ main.c
@@ -1,32 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Usuario.h"
-#define TAMSERIE 20
-#define TAMUSUARIO 100
-
-
+#include "estructuragenerica.h"
+#include "string.h"
+#define CANTIDAD 10
 int main()
 {
-    eSerie listaDeSeries[TAMSERIE];
-    eUsuario listaDeUsuarios[TAMUSUARIO];
+     char seguir ='s';
+     int opcion=0;
 
-    inicializarSeriesEstado(listaDeSeries, TAMSERIE);
-    inicializarSeriesHardCode(listaDeSeries);
+    eGenerica listadoGenerico[CANTIDAD];
+    eGen_init(listadoGenerico,CANTIDAD);
 
-    inicializarUsuariosEstado(listaDeUsuarios,TAMUSUARIO);
-    inicializarUsuariosHardCode(listaDeUsuarios);
+     do
+        {
+            printf("1: Alta de usuario \n");
+            printf("2: Modificar datos del usuario \n");
+            printf("3: Baja del usuario \n");
+            printf("4: Publicar producto \n");
+            printf("5: Modificar publicacion \n");
+            printf("6: Cancelar publicacion \n");
+            printf("7: Comprar producto \n");
+            printf("8: Listar publicaciones del usuario \n");
+            printf("9: Listar publicaciones \n");
+            printf("10: Listar usuarios \n");
+            scanf("%d",&opcion);
 
-    mostrarListaSeries(listaDeSeries,TAMSERIE);
-    mostrarListaUsuarios(listaDeUsuarios,TAMUSUARIO);
+                    switch (opcion)
+                {
+                    case 1:
+                            eGen_buscarLugarLibre(listadoGenerico,CANTIDAD);
+                            eGen_alta(listadoGenerico,CANTIDAD);
+                            eGen_mostrarListado(listadoGenerico,CANTIDAD);
+                            break;
 
-    mostrarUsuarioConSuSerie(listaDeUsuarios,TAMUSUARIO,listaDeSeries,TAMSERIE);
-/*  1. Mostrar el listado de series
-    2. Mostrar el listado de usuarios
-    3. Mostrar el listado de Usuarios con el nombre de la serie que ve
-    4. Mostrar por cada serie, el nombre de los usuarios que la ven.
+                    /*case 2:
+                            eGen_modificacion(listadoGeneroco,CANTIDAD);
+                            break;
+                    case 3:
+                            eGen_baja(ListadoGenerico,CANTIDAD);
+                           break;
+
+                    default:
+                            printf("ingrese una opcion correcta");
+                }
+
+    eGenerica listadoGenerico[CANTIDAD];
+    printf( "\n \t muestro el array... esta vacio");
+    eGen_mostrarListado(listadoGenerico,CANTIDAD);
+    getchar();
+
+    printf( "\n \t muestro el array con los borrados y vacios");
+    eGen_mostrarListadoConBorrados(listadoGenerico,CANTIDAD);
+    //getchar();
+
+    //eGen_alta(listadoGenerico,CANTIDAD);
+    //eGen_alta(listadoGenerico,CANTIDAD);
+    //printf( "\n \t cargo dos elementos y muestro el array");
+    //eGen_mostrarListado(listadoGenerico,CANTIDAD);
+    //getchar();
+
+    //printf( "\n \t muestro el array con los borrados y vacios");
+    //eGen_mostrarListadoConBorrados(listadoGenerico,CANTIDAD);
+    //getchar();
 */
 
-
-
-    return 0;
+                }
+        }while(opcion!=5);
+    return 0 ;
 }
